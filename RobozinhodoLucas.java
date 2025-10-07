a0b19d59049ada5c2cc927a547add44580947294
Lucas Pacheco <lpachecokempfer@gmail.com>
2025-10-07 19:40:59 -0300

    primeiro commit

---
+++ RobozinhodoLucas.java
@@ -0,0 +1,28 @@
+package robos;
+import robocode.*;
+
+public class RobozinhodoLucas extends Robot
+{
+	
+	public void run() {
+		
+		while(true) {
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
+
+	public void onHitByBullet(HitByBulletEvent e) {
+		back(10);
+	}
+	
+	public void onHitWall(HitWallEvent e) {
+		back(20);
+	}	
+}
